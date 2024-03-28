66b6e73a5762033ec8094fe028c74e8797f8b4ca
hiu lam tse <hiulam1@hotmail.com>
2024-03-28 16:15:15 +0100

    added read me

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useWindowDimensions } from "react-native";
+import { Canvas, RoundedRect } from "@shopify/react-native-skia";
+import {
+  useImage,
+  Image,
+  Blur,
+  Fill,
+  Group,
+  Paint,
+  Shadow,
+  BoxShadow,
+  usePaintRef,
+  rrect,
+  DiffRect,
+  rect,
+  box,
+} from "@shopify/react-native-skia";
+import { useMemo } from "react";
 export default function App() {
+  const { width, height } = useWindowDimensions();
+  const skwidth = 256;
+  const skheight = 256;
+  const x = width / 2 - skwidth / 2;
+  const y = height / 2 - skheight / 2;
+  const radius = 10;
+  const color = "rgba(255, 255, 255, 0.3)";
+
+  const image = useImage(require("./guille-pozzi-g3esK1uXCjM-unsplash.jpg"));
+  const inner = rrect(rect(x, y, skwidth, skheight), 10, 10);
+  const outer = rrect(rect(x - 1, y - 1, skwidth + 1, skheight + 1), 10, 10);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas style={{ flex: 1 }}>
+      <Image
+        image={image}
+        fit="cover"
+        x={0}
+        y={0}
+        width={width}
+        height={height}
+      />
+      <DiffRect inner={inner} outer={outer} color="white" />
+      <Group clip={inner}>
+        <Blur blur={2} />
+        <Image
+          image={image}
+          fit="cover"
+          x={0}
+          y={0}
+          width={width}
+          height={height}
+        />
+      </Group>
+      <Group>
+        <RoundedRect
+          x={x - 1}
+          y={y - 1}
+          width={skwidth + 2}
+          height={skheight + 2}
+          r={radius}
+          color={color}
+        >
+          <Shadow dx={20} dy={20} blur={20} color="#293c45" />
+          <Shadow dx={-20} dy={-20} blur={10} color="#939ca0" />
+        </RoundedRect>
+      </Group>
+    </Canvas>
+    // <Canvas style={{ width: 256, height: 256 }}>
+    //   <Fill color="lightblue" />
+    //   <RoundedRect
+    //     x={32}
+    //     y={32}
+    //     width={192}
+    //     height={192}
+    //     r={32}
+    //     color="lightblue"
+    //   >
+    //     <Shadow dx={12} dy={12} blur={25} color="#93b8c4" />
+    //     <Shadow dx={-12} dy={-12} blur={25} color="#c7f8ff" />
+    //   </RoundedRect>
+    // </Canvas>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Glassmorphism
+# Glassmorphism Card with React Native Skia
+
+This project demonstrates a card designed in the style of glassmorphism, utilizing React Native Skia for the rendering. The glassmorphism style emulates the look of frosted glass and provides a modern, aesthetic touch to UI components.
+
+## Setup
+
+To get started with this project, follow these steps:
+
+### Prerequisites:
+
+- Ensure you have [Expo](https://expo.dev/) installed on your system. Expo is a framework and platform for universal React applications.
+- Install React Native Skia. React Native Skia brings the power of Skia, a high-performance 2D graphics library, to React Native. For installation instructions, refer to the [React Native Skia documentation](https://shopify.github.io/react-native-skia/).
+
+### Clone and Run:
+
+- Clone this repository to your local device.
+- Navigate to the project directory in your terminal.
+- Run `expo start` to start the project. This will open up a development server, and you can then view the project in the Expo Go app on your mobile device or in a simulator/emulator on your computer.
+
+Enjoy the sleek look of glassmorphism in your React Native applications!
